4c792d2bd1c9d31cd22aa40833301391e49172df
Chris G <cmgomez1008@gmail.com>
2026-01-20 11:34:45 -0800

    fyp defined and routed to well

--- backend/app/controllers/fyp_controller.rb
+++ backend/app/controllers/fyp_controller.rb
@@ -1,4 +1,19 @@
 class FypController < ApplicationController
   def index
   end
+
+  def login
+    result = MlApiService.login(
+      username: params[:username],
+      password: params[:password]
+    )
+
+    if result['user']
+      session[:user] = result['user']
+      redirect_to fyp_index_path, notice: "Welcome back, #{result['user']['username']}!"
+    else
+      flash.now[:alert] = result[:error] || "Invalid username or password"
+      render :index, status: :unprocessable_entity
+    end
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  post "fyp/login", to: "fyp#login"
   get "fyp/index"
   root "home#index"
   get "home/login"
