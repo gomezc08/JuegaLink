ae75daf26448cab82b72a16199498bec0c5a09e5
Chris G <cmgomez1008@gmail.com>
2026-01-20 11:09:28 -0800

    header bar modified

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :current_user, :user_signed_in?
+
+  def current_user
+    @current_user ||= session[:user] if session[:user]
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "home/login", to: "home#login_post"
   get "home/signup"
   post "home/signup", to: "home#signup_post"
+  delete "home/logout", to: "home#logout", as: :home_logout
 
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
