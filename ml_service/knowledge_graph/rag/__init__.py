50a0a94ab14438a42ba2068c18872011a3313c40
Chris G <cmgomez1008@gmail.com>
2026-02-04 16:03:31 -0800

    defined data folder

--- ml_service/knowledge_graph/rag/__init__.py
+++ ml_service/knowledge_graph/rag/__init__.py
@@ -1,8 +1,8 @@
-from .rag_chain import RAGChain
-from .document_ingestion import DocumentIngestion
+from . import document_ingestion
 from .graph_builder import GraphBuilder
 from .nodes import Nodes
+from .rag_chain import RAGChain
 from .state import State
 from .vector_store import VectorStore
 
-__all__ = ["RAGChain", "DocumentIngestion", "GraphBuilder", "Nodes", "State", "VectorStore"]
+__all__ = ["document_ingestion", "GraphBuilder", "Nodes", "RAGChain", "State", "VectorStore"]
